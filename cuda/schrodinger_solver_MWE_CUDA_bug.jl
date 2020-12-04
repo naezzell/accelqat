04f97c39c9b169434871cac39fc0e8e101860085
naezzell <naezzell@gmail.com>
2020-12-04 14:51:36 -0800

    fixed the second cpu_example to be gpu_example

--- cuda/schrodinger_solver_MWE_CUDA_bug.jl
+++ cuda/schrodinger_solver_MWE_CUDA_bug.jl
@@ -40,15 +40,15 @@ function cpu_example(n)
 end
 
 
-function cpu_example(n)
+function gpu_example(n)
     tf = 10
     u0 = complex(ones(2^n))
-    u0 = u0 / norm(u0)
+    u0 = cu(u0 / norm(u0))
     # generate random 2^n x 2^n positive matrix (with complex entires)
     a0 = rand(2^n, 2^n) + 1.0im * rand(2^n, 2^n)
-    H0 = conj(transpose(a0)) * a0
+    H0 = cu(conj(transpose(a0)) * a0)
     a1 = rand(2^n, 2^n) + 1.0im * rand(2^n, 2^n)
-    H1 = conj(transpose(a0)) * a0
+    H1 = cu(conj(transpose(a0)) * a0)
 
     function interpolate_H(t)
         """
@@ -67,9 +67,9 @@ function cpu_example(n)
         @inbounds axpy!(-1.0im, current_H, A)
     end
 
-    diff_op = DiffEqArrayOperator(-1.0im*copy(H0), update_func = update_func!)
+    diff_op = cu(DiffEqArrayOperator(-1.0im*copy(H0), update_func = update_func!))
     jac_cache = similar(H0)
-    jac_op = DiffEqArrayOperator(jac_cache, update_func = update_func!)
+    jac_op = cu(DiffEqArrayOperator(jac_cache, update_func = update_func!))
     ff = ODEFunction(diff_op, jac_prototype = jac_op)
 
     prob = ODEProblem{true}(ff, u0, (0.0, tf))
